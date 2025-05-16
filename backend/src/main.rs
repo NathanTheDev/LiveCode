717a23bc2b6f23e1f62ec6f5f2da558dd09e25a0
Nathan <nathan.smith1922@gmail.com>
2025-05-16 14:51:24 +1000

    Implemented sending a receiving a message from frontend to backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,28 +1,50 @@
-use axum::{Json, Router, Server, routing::get};
-use serde::Serialize;
-use std::net::SocketAddr;
+use axum::{
+    Json, Router, Server,
+    extract::State,
+    routing::{get, put},
+};
+use serde::{Deserialize, Serialize};
+use std::{
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+};
 use tower_http::cors::{Any, CorsLayer};
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct Message {
     content: String,
 }
 
-async fn hello() -> Json<Message> {
-    Json(Message {
-        content: "It's Working!!!".to_string(),
-    })
+async fn hello_get(State(state): State<Arc<Mutex<Message>>>) -> Json<Message> {
+    let message = state.lock().unwrap().clone();
+    Json(message)
+}
+
+async fn hello_put(
+    State(state): State<Arc<Mutex<Message>>>,
+    Json(payload): Json<Message>,
+) -> Json<Message> {
+    let mut message = state.lock().unwrap();
+    message.content = payload.content.clone();
+    Json(message.clone())
 }
 
 #[tokio::main]
 async fn main() {
-    // very permissive CORS (dev only)
     let cors = CorsLayer::new()
         .allow_origin(Any)
         .allow_methods(Any)
         .allow_headers(Any);
 
-    let app = Router::new().route("/api/hello", get(hello)).layer(cors);
+    let shared_msg = Arc::new(Mutex::new(Message {
+        content: "Enter a message".to_string(),
+    }));
+
+    let app = Router::new()
+        .route("/api/message", get(hello_get))
+        .route("/api/message", put(hello_put))
+        .with_state(shared_msg)
+        .layer(cors);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     println!("Backend running at http://{}", addr);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,20 +4,45 @@ import axios from 'axios';
 function App() {
   const [message, setMessage] = useState('Loading...');
 
+  const [input, setInput] = useState('');
+
   useEffect(() => {
-    axios.get('http://localhost:3000/api/hello')
-      .then(res => {
-        setMessage(res.data.content);
-      })
-      .catch(err => {
-        setMessage('Error fetching message');
-        console.error(err);
-      });
+    getMessage();
   }, []);
+  
+  const getMessage = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/api/message');
+  
+      setMessage(response.data);
+    } catch (error) {
+      console.error('Error sending GET request:', error);
+    }
+  };
+
+  const handleSend = async () => {
+    try {
+      const response = await axios.put('http://localhost:3000/api/message', {
+        content: input,
+      });
+      console.log("Server Response:", response.data);
+      getMessage();
+    } catch (error) {
+      console.error('Error sending PUT request:', error);
+    }
+
+  }
 
   return (
-    <div style={{ padding: 20 }}>
-      <h1>{message}</h1>
+    <div>
+      <h1>{message.content}</h1>
+      <input type="text" value={input} placeholder='Enter text to be sent to backend' onChange={(e) => {
+        setInput(e.target.value);
+      }}/>
+      <button onClick={() => {
+        handleSend();
+        setInput("");
+      }}>Send</button>
     </div>
   );
 }
