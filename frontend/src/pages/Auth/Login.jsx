0a4ef38bbbccf8f5681938edeb8a1b400cb2441d
Nathan <nathan.smith1922@gmail.com>
2025-06-18 23:24:50 +1000

    Implemented google OAuth redirect

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,11 +1,26 @@
+import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from 'jwt-decode';
+import { useNavigate } from 'react-router';
 import './Login.css'
 
 const Login = () => {
-	return (
-		<div className='login-container'>
-			<h1>Please Login To Access The IDE</h1>
-		</div>
-	);
-};
+	const navigate = useNavigate();
+
+  return (
+	<div className='login-container'>
+		<h1>Please Login to Access the IDE</h1>
+		<GoogleLogin
+			onSuccess={credentialResponse => {
+				const decoded = jwtDecode(credentialResponse.credential);
+				console.log("Succeeded");
+				navigate('/IDE');
+			}}
+			onError={() => {
+				console.error("Login Failed");
+			}}
+		/>
+	</div>
+  );
+}
 
 export default Login;
