91d53cd9d030950a96e5707955730a0847a0fbde
Nathan <nathan.smith1922@gmail.com>
2025-07-02 15:27:05 +1000

    Implemented basic firebase register

---
+++ frontend/src/components/FullInput.jsx
@@ -0,0 +1,17 @@
+const FullInput = ({ placeholder, textType, labelText, editFn }) => {
+	return (
+		<div className="pb-4">
+			<label className="block text-sm font-medium text-gray-500">{labelText}</label>
+			<input
+				type={textType}
+				className="mt-1 w-full rounded-md border border-gray-300 px-3 py-2 focus:outline-none bg-[#282C34]"
+				placeholder={placeholder}
+				onChange={(e) => {
+					editFn(e.target.value);
+				}}
+			/>
+		</div>
+	);
+};
+
+export default FullInput;
--- frontend/src/pages/Pages.jsx
+++ frontend/src/pages/Pages.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Navigate } from "react-router";
 import Login from "./Auth/Login.jsx";
+import Register from "./Auth/Register.jsx";
 import IDE from "./IDE.jsx";
 
 const Pages = () => {
   return (
     <Routes>
+      <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/IDE" element={<IDE />} />
+      <Route path="/ide" element={<IDE />} />
       <Route path="/*" element={<Login />} />
     </Routes>
   );
